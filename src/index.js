9fef863005746adb106e19b580fff2d3533bbb7a
erbonilla <erbonilla@outlook.com>
2024-12-17 17:18:28 -0600

    add authentication pages for user registration and login

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import ProductList from "./pages/admin/products/ProductList";
 import CreateProduct from './pages/admin/products/CreateProduct';
 import EditProduct from './pages/admin/products/EditProduct'; 
 import ProductDetails from "./pages/ProductDetails";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
@@ -18,6 +20,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/products/:id" element={<ProductDetails />} />
+
+
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/login" element={<Login />} />
+
+
         <Route path="/admin/products" element={<ProductList />} />
         <Route path="/admin/products/create" element={<CreateProduct />} />
         <Route path="/admin/products/edit:id" element={<EditProduct />} />

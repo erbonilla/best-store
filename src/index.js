37ea50b56d2fa8e125a61ed06ff85d4bba7802f0
erbonilla <erbonilla@outlook.com>
2024-12-17 18:21:29 -0600

    refactor App component to include AppContext for user credentials management and fix Router structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import { Footer, Navbar } from "./components/layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -11,28 +11,44 @@ import EditProduct from './pages/admin/products/EditProduct';
 import ProductDetails from "./pages/ProductDetails";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import { AppContext } from "./AppContext";
 
 function App() {
+
+  function getStoredCredentials() {
+      let data = localStorage.getItem("credentials")
+      if (data) {
+          let json = JSON.parse(data)
+          return json
+      }
+      return null
+  }
+
+  const [userCredentials, setUserCredentials] = useState(getStoredCredentials())
+
+  useEffect(() => {
+    let str = JSON.stringify(userCredentials)
+    localStorage.setItem("credentials", str)
+}, [userCredentials])
+
   return (
-    <Router>
+    <AppContext.Provider value={{ userCredentials, setUserCredentials }}>
+    <BrowserRouter>
       <Navbar />
-      <Routes>
+      <BrowserRouter>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-
-
         <Route path="/auth/register" element={<Register />} />
         <Route path="/auth/login" element={<Login />} />
-
-
         <Route path="/admin/products" element={<ProductList />} />
         <Route path="/admin/products/create" element={<CreateProduct />} />
         <Route path="/admin/products/edit:id" element={<EditProduct />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </BrowserRouter>
       <Footer />
-    </Router>
+    </BrowserRouter>
+    </AppContext.Provider> 
   );
 }
 
